a82e0ce7c1ca8678469beaf0584354de17ea6f05
Thomas Jetzinger <thomas@jetzinger.com>
2013-01-30 12:24:29 +0100

    Meta Data Extension

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-import os
+from setuptools import setup, find_packages
 from mezzanine_recipes import __version__
 
 setup(
@@ -7,15 +6,29 @@ setup(
     version=__version__,
     author='Thomas Jetzinger',
     author_email='thomas@jetzinger.com',
-    description='Recipe pages for the Mezzanine CMS',
+    description='Recipe blog posts for Mezzanine CMS',
     url='https://github.com/tjetzinger/mezzanine-recipes',
-    packages=['.'.join(i[0].split(os.sep))
-              for i in os.walk('mezzanine_events')
-              if '__init__.py' in i[2]
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=[
+        'setuptools_git>=0.3',
     ],
     install_requires=[
-        #'Mezzanine==1.2.4',
-        #'django-tastypie==0.9.12-alpha',
+        'Mezzanine==1.3.0',
+        'django-tastypie==0.9.12-alpha',
+        'South==0.7.6',
         'uuid==1.30',
+    ],
+    dependency_links = [
+        'http://github.com/toastdriven/django-tastypie/tarball/master#egg=django-tastypie-0.9.12-alpha',
+    ],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
